ea3ef09698da390e6146fefc5b57cdb81530a4e5
Dragon <codingdragon@proton.me>
2024-05-17 21:07:20 -0700

    Changed code to calculate correct square size based on the amount of grid squares wanted (i.e 16x16, 64x64).

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const pad = document.querySelector(".pad");
+let size = 64;
 
-
-for (let i = 0; i < 16 * 16; i++) {
+for (let i = 0; i < size * size; i++) {
     const box = document.createElement("div");
     box.addEventListener("mouseover", function () {
         box.style.backgroundColor = "#222";
     });
 
-    box.style.height = "60px";
-    box.style.minWidth = "60px";
+    let conversion = 960 / size;
+
+    box.style.height = `${conversion}px`;
+    box.style.minWidth = `${conversion}px`;
     box.style.border = "solid #222"
     box.style.borderWidth = "0 1px 1px 0px";
     pad.append(box);
